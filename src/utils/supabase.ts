2cfb703898f3d33b160345377a60417857765455
Sopheak PHEAP <p.sopheak50@outlook.com>
2025-07-12 00:47:09 +0700

    remove console

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -3,8 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = "https://dtoironstdvrgabmcifp.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0b2lyb25zdGR2cmdhYm1jaWZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY5NDg2MDYsImV4cCI6MjA1MjUyNDYwNn0.cLq0L3b5gx82cTTtZxGMxY6I4RdzBL5nOOrPIyUy4WM";
 
-console.log(supabaseUrl, supabaseKey)
-
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase environment variables');
 }
